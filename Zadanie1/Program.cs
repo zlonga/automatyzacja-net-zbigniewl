50ad94cd4a70a8dfde0ad40cdec3f19885885f1e
Zbigniew Longa <zbigniew.longa@luxmed.pl>
2017-11-22 22:38:52 +0100

    On branch master

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "";
+
+            Console.WriteLine("Witaj świecie!");
+            while (x != "exit")
+            { Console.WriteLine("Jeśli chcesz zakończyć wpisz 'exit' i naciśnij ENTER");
+                x = Console.ReadLine();
+            }
+        }
+    }
+}
