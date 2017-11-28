dec3aea76614f23e47e89414574bf69c1bf23b1f
unknown <zbigniew.longa@luxmed.pl>
2017-11-28 17:47:35 +0100

    komentarze

--- Automatyzacja2017/MySelenium/Example.cs
+++ Automatyzacja2017/MySelenium/Example.cs
@@ -41,28 +41,29 @@ namespace SeleniumTests
 
         {
 
-            driver.Navigate().GoToUrl(baseURL);
-            driver.FindElement(By.Id("lst-ib")).Clear();
-            driver.FindElement(By.Id("lst-ib")).SendKeys("code sprinters");
-            driver.FindElement(By.Id("lst-ib")).Submit();
-            driver.FindElement(By.LinkText("Code Sprinters -")).Click();
+            driver.Navigate().GoToUrl(baseURL);                                                                     //otwiera stronę startową
+            driver.FindElement(By.Id("lst-ib")).Clear();                                                            //czyści pole o Id
+            driver.FindElement(By.Id("lst-ib")).SendKeys("code sprinters");                                         //wprowadza tekst
+            driver.FindElement(By.Id("lst-ib")).Submit();                                                           //zatwierdza
+            driver.FindElement(By.LinkText("Code Sprinters -")).Click();                                            //wyszukuje link i w niego klika
 
-            var element = driver.FindElement(By.LinkText("Poznaj nasze podejście"));
+            var element = driver.FindElement(By.LinkText("Poznaj nasze podejście"));                                //test czy na stronie widoczny jest link z tekstem
             Assert.NotNull(element);
-            var elements = driver.FindElements(By.LinkText("Poznaj nasze podejście"));
+            var elements = driver.FindElements(By.LinkText("Poznaj nasze podejście"));                              //czy na stronie widoczny jest jeden link
             Assert.Single(elements);
             // Assert.Equal("Code Sprinters -", driver.Title);
-            driver.FindElement(By.LinkText("Akceptuję")).Click();
-            //waitForElementPresent(By.LinkText("Poznaj nasze podejście"), 2); //czeka max 2s
-            //Thread.Sleep(2000); //czeka na sztywno 2s
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
-            wait.Until(ExpectedConditions.InvisibilityOfElementWithText(By.LinkText("Akceptuję"), "Akceptuję"));
-            driver.FindElement(By.LinkText("Poznaj nasze podejście")).Click();
-            var elements_2 = driver.FindElements(By.TagName("h2"));
-            Assert.Single(elements_2.Where(tag => tag.Text == "WIEDZA NA PIERWSZYM MIEJSCU"));
+            driver.FindElement(By.LinkText("Akceptuję")).Click();                                                   //wyszukuje link i klika w niego
+            //waitForElementPresent(By.LinkText("Poznaj nasze podejście"), 2);                                      //czeka max 2s
+            //Thread.Sleep(2000);                                                                                   //czeka na sztywno 2s
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));                                //czeka max 5s 
+            wait.Until(ExpectedConditions.InvisibilityOfElementWithText(By.LinkText("Akceptuję"), "Akceptuję"));    //lub do chwili aż zniknie link Akceptuję
+            driver.FindElement(By.LinkText("Poznaj nasze podejście")).Click();                                      //wyszukuje link z tekste i kilka w niego
+            var elements_2 = driver.FindElements(By.TagName("h2"));                                                 //wyszukuje wszystkie teksty z tagiem h2
+            Assert.Single(elements_2.Where(tag => tag.Text == "WIEDZA NA PIERWSZYM MIEJSCU"));                      //sprawdza czy 1 element w powyższej liscie ma tekst ""
 
-            Assert.Contains("WIEDZA NA PIERWSZYM MIEJSCU", driver.PageSource);
+            Assert.Contains("WIEDZA NA PIERWSZYM MIEJSCU", driver.PageSource);                                      //sprawdza czy na stronie jest text
         }
+
         protected void waitForElementPresent(By by, int seconds)
 
         {
